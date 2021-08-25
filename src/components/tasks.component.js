dca1b8326deca3468830b1cd136fb905020fe770
Dominykas Sipelis <DominykasSipelis@gmail.com>
2021-08-25 16:51:37 -0500

    added basic task table

--- src/components/tasks.component.js
+++ src/components/tasks.component.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 
 import UserService from "../services/user.service";
 
-export default class BoardUser extends Component {
+export default class Tasks extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            content: ""
+            content: "",
+            tasks: []
         };
     }
 
@@ -29,15 +30,75 @@ export default class BoardUser extends Component {
                 });
             }
         );
+        this.updateTasks();
+        this.updateTimer = setInterval(() => this.updateTasks(), 30000);
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.updateTimer);
+    }
+
+    updateTasks(){
+        UserService.getTasks().then(
+            response => {
+                this.setState({
+                    tasks: response.data
+                })
+            },
+            error => {
+                this.setState({
+                    tasks:
+                        (error.response &&
+                            error.response.data &&
+                            error.response.data.message) ||
+                        error.message ||
+                        error.toString()
+                })
+            }
+        );
+    }
+
+    renderTasks(){
+        return this.state.tasks.map((task) => {
+            const { desc, endTime, frequency, id, lastTime, name, repeating, start} = task
+            return(
+                <tr key={id}>
+                    <th scope={"row"}>{id}</th>
+                    <td>{name}</td>
+                    <td>{desc}</td>
+                    <td>{start}</td>
+                </tr>
+            )
+        });
     }
 
     render() {
         return (
-            <div className="container">
-                <header className="jumbotron">
-                    <h3>{this.state.content}</h3>
-                </header>
+            <div className="tasks-table">
+                <div className="container">
+                    <header className="jumbotron align-content-center">
+                        <h3>Task List</h3>
+                    </header>
+                </div>
+                <table>
+                    <thead>
+                    <tr>
+                        <th scope={"col"}>id</th>
+                        <th scope={"col"}>name</th>
+                        <th scope={"col"}>description</th>
+                        <th scope={"col"}>start time</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.renderTasks()}
+                    </tbody>
+                </table>
             </div>
+            // <div className="container">
+            //     <header className="jumbotron">
+            //         <h3>{this.state.content}</h3>
+            //     </header>
+            // </div>
         );
     }
 }
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,19 +1,24 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:8080/api/test/';
+const TEST_API_URL = 'http://localhost:8080/api/test/';
+const API_URL = 'http://localhost:8080/api/task';
 
 class UserService {
     getPublicContent() {
-        return axios.get(API_URL + 'all');
+        return axios.get(TEST_API_URL + 'all');
     }
 
     getUserBoard() {
-        return axios.get(API_URL + 'user', { headers: authHeader() });
+        return axios.get(TEST_API_URL + 'user', { headers: authHeader() });
     }
 
     getAdminBoard() {
-        return axios.get(API_URL + 'admin', { headers: authHeader() });
+        return axios.get(TEST_API_URL + 'admin', { headers: authHeader() });
+    }
+
+    getTasks(){
+        return axios.get(API_URL, {headers: authHeader()});
     }
 }
 
